3f0f3b1d07a75c3728679e514ab0654b9538f1df
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-07-30 16:19:38 -0400

    re-factored fundME and price converter smart contracts

--- Lesson 7/hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
+++ Lesson 7/hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -17,4 +17,15 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    // What is mocking?
+    // This is primarily used for unit testing. An object under test may have dependencies
+    // on other objects. To isolate the behaviour of the object you want to replace the other
+    // objects by mocks that simulate the behaviour of the real objects. This is useful if the
+    // real objects are impractical to incorporate into the unit test.
+
+    //  WHEN GOING FOR LOCALHOST OR HARDHAT NETWORK WE WANT TO USE A MOCK
+
+    const fundMe = await deploy("FundMe")
 }
