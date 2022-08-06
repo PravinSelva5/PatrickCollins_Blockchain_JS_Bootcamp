e57187ae3c4ed86799e5b1d490ee8eb072a247f1
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-08-06 17:51:29 -0400

    unit test updates

--- Lesson 7/hardhat-fund-me-fcc/test/unit/FundMe.test.js
+++ Lesson 7/hardhat-fund-me-fcc/test/unit/FundMe.test.js
@@ -84,9 +84,42 @@ describe("FundMe", async function () {
         })
 
         it("allows us to withdraw with multiple funders", async function () {
+            // Arrange
             const accounts = await ethers.getSigners()
-            for (let i = 1; i < 6; i++){
-                const fundMeConnectedContract
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                )
+                fundMeConnectedContract.fund({ value: sendValue })
+                const startingFundMeBalance = await fundMe.provider.getBalance(
+                    fundMe.address
+                )
+                const startingDeployerBalance =
+                    await fundMe.provider.getBalance(deployer)
+            }
+            // Act
+            const transactionResponse = await fundMe.withdraw()
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            // Assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                // because startingFundMeBalance is from the blockchain, which will result in Big Number types (are objects)
+                // Review solidity docs for more information.
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            // Make sure that the funders are reset properly
+            await expect(fundMe.funders(0)).to.be.reverted
+
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address),
+                    0
+                )
             }
         })
     })
