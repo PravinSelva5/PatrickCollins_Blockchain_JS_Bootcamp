6716d47889714c60e21410a50bfad72a68c5541b
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-08-12 16:44:31 -0400

    completed fund me smart contract, adding script commands to package.json file

--- Lesson 7/hardhat-fund-me-fcc/test/staging/FundMe.staging.test.js
+++ Lesson 7/hardhat-fund-me-fcc/test/staging/FundMe.staging.test.js
@@ -1,12 +1,25 @@
 const { getNamedAccounts, ethers } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+const { assert, expect } = require("chai")
 
-describe("FundMe", async function () {
-    let fundMe
-    let deployer
-    const sendValue = ethers.utils.parseEther("1")
+developmentChains.includes(network.name)
+    ? describe.skip()
+    : describe("FundMe", async function () {
+          let fundMe
+          let deployer
+          const sendValue = ethers.utils.parseEther("1")
 
-    beforeEach(async function () {
-        deployer = (await getNamedAccounts()).deployer
-        fundMe = await ethers.getContract("FundMe", deployer)
-    })
-})
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+
+          it("allows people to fund and withdraw", async function () {
+              await fundMe.fund({ value: sendValue })
+              await fundMe.withdraw()
+              const endingBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              assert.equal(endingBalance.toString(), "0")
+          })
+      })
