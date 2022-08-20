026c8bf2c0a0467c92be7976c9b8cff203334018
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2022-08-19 22:59:30 -0400

    working through the frontend portion of fundme app. Working on receviing transaction confirmation post fund me button clicks

--- Lesson 8/html-fund-me-fcc/index.js
+++ Lesson 8/html-fund-me-fcc/index.js
@@ -2,9 +2,10 @@
 // import keyword is used
 
 import { ethers } from "./ethers05.6.esm.min.js"
+import { abi } from "./constants.js"
 
-const connectButton = document.getElementById("connect")
-const fundButton = document.getElementById("fund")
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
 connectButton.onclick = connect
 fundButton.onclick = fund
 
@@ -30,7 +31,34 @@ async function fund(ethAmount) {
         //      provider / connection to the blockchain
         //      signer / wallet / someone with some ETH to pay gas fees
         //      contract that we are interacting with: ABI & address needed
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        console.log(signer)
+        const contract = "" // could not run this correctly due to an error in hardhat package.json
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            // we can listen for the tx to be mined
+            // we can listen for an event
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (e) {
+            console.log(e)
+        }
     }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}...`)
+
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+        console.log(
+            `Completed with ${transactionReceipt.confirmations} confirmations`
+        )
+    })
+    // return new Promise
+    // the reason we're going to return a promise is we would like to have a listener monitoring the blockchain
+    // ethers comes with a way to listen to events
 }
 
 // fund function
